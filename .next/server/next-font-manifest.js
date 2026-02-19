ab3eb8616325a58c142c897fa402cbdb4a1df906
sbkokesmer <sbkokesmer@gmail.com>
2026-02-19 15:15:40 +0300

    Stop tracking Next.js build artifacts

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/elif/Downloads/ensokkenbuild 2 uyumlu/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"EKpdQ1sp0H8oHsNX6UFPlL0rLPpTlPhz6keANyg5LZQ=\"}"
